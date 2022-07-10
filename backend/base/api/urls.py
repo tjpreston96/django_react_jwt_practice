8ddcbf7c1cf26cdd018c5a90dcc2d1c035f863fe
Tyler Preston <tpreston96@gmail.com>
2022-07-09 23:19:22 -0500

    update path for token claims

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from . import views
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path("", views.get_routes, name="routes"),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
