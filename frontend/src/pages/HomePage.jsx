fd26abd5149b4a967e10020905daf3f798107be9
Tyler Preston <tpreston96@gmail.com>
2022-07-10 18:28:16 -0500

    HomePage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <p>You are logged in and!</p>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default HomePage;
