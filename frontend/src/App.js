4a51b0e1ef6f9bebadd53045cd7bc5b866f3e4cb
Tyler Preston <tpreston96@gmail.com>
2022-07-10 18:30:07 -0500

    Routes, PrivateRoutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import Header from "./components/Header";
+import PrivateRoutes from "./utils/PrivateRoutes";
 
 function App() {
   return (
     <div className="App">
-      <p>Home</p>
+      <Router>
+        {/* Static Components */}
+        <Header />
+
+        <Routes>
+          {/* Public Routes */}
+          <Route element={<LoginPage />} path="/login" />
+
+          {/* Private Routes */}
+          <Route element={<PrivateRoutes />}>
+            <Route element={<HomePage />} path="/" />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
