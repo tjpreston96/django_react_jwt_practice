ddc0b6d579b2559edc12954d776c2beeb97a8563
Tyler Preston <tpreston96@gmail.com>
2022-07-11 19:39:41 -0500

    Rename PrivateWrapper

--- frontend/src/utils/PrivateWrapper.js
+++ frontend/src/utils/PrivateWrapper.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-const PrivateRoutes = () => {
+const PrivateWrapper = () => {
   let isAuthenticated = false;
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
-export default PrivateRoutes;
+export default PrivateWrapper;
