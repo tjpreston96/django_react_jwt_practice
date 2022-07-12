3ffec662b0685e49d0a4cc780f00788c48c8144a
Tyler Preston <tpreston96@gmail.com>
2022-07-11 20:14:04 -0500

    look for user in PrivateWrapper

--- frontend/src/utils/PrivateWrapper.js
+++ frontend/src/utils/PrivateWrapper.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const PrivateWrapper = () => {
-  let isAuthenticated = false;
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  const { user } = useContext(AuthContext).contextData;
+  return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateWrapper;
