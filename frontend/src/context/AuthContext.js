e8b2fbb5fa1fb79a39d8c6cd073a15deffb86f9c
Tyler Preston <tpreston96@gmail.com>
2022-07-11 19:27:59 -0500

    loginUser

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,37 @@
+import { useState, createContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [authTokens, setAuthTokens] = useState(null);
+
+  let loginUser = async (e) => {
+    e.preventDefault();
+    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    });
+    let data = await response.json();
+    console.log("data:", data);
+    console.log("response:", response);
+  };
+
+  let contextData = {
+    loginUser: loginUser,
+  };
+
+  return (
+    <AuthContext.Provider value={{ contextData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
