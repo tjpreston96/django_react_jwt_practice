2ff2b22113f81a291834331965b2e59972d774b4
Tyler Preston <tpreston96@gmail.com>
2022-07-11 19:28:10 -0500

    loginUser

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 const LoginPage = () => {
+  let {loginUser} = useContext(AuthContext).contextData;
+  
+
   return (
     <div>
-      <form>
+      <form onSubmit={loginUser}>
         <input type="text" name="username" placeholder="Enter Username" />
-        <input type="text" name="password" placeholder="Enter Password" />
+        <input type="password" name="password" placeholder="Enter Password" />
         <input type="submit" />
       </form>
     </div>
