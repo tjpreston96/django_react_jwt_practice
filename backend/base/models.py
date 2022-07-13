ed70b5528f71f75e43388f7cb133561893659243
Tyler Preston <tpreston96@gmail.com>
2022-07-12 22:10:35 -0500

    fix typo on model

--- backend/base/models.py
+++ backend/base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    body = models.Textfield(blank=True, null=True)
+    body = models.TextField(blank=True, null=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
