95a0f1e4ddf623dec57e09ce378c850b7807ec51
Tyler Preston <tpreston96@gmail.com>
2022-07-12 22:46:19 -0500

    protected endpoint

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -1,9 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import NoteSerializer
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -25,14 +28,18 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def get_routes(request):
     routes = [
         {"Endpoint": "/api/token", "Method": "POST", "Description": ""},
-        {"Endpoint": "/api/token/refresh", "Method": "POST", "Description": "Allows for Token refresh within 60 days of last login"},
+        {
+            "Endpoint": "/api/token/refresh",
+            "Method": "POST",
+            "Description": "Allows for Token refresh within 60 days of last login",
+        },
     ]
 
     return Response(routes)
 
 
 @api_view(["GET"])
-
+@permission_classes([IsAuthenticated])
 def get_notes(request):
     user = request.user
     notes = user.note_set.all()
