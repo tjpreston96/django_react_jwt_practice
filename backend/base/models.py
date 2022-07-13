28d321177631c97840a80de9dba04178b4905458
Tyler Preston <tpreston96@gmail.com>
2022-07-12 22:07:08 -0500

    Note model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    body = models.Textfield(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
