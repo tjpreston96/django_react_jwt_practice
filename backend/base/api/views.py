eaa84fbd585ced6faa416c482bca4f81d28599f8
Tyler Preston <tpreston96@gmail.com>
2022-07-12 22:41:09 -0500

    get_notes and update description for token refresh

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from .serializers import NoteSerializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -24,7 +25,16 @@ class MyTokenObtainPairView(TokenObtainPairView):
 def get_routes(request):
     routes = [
         {"Endpoint": "/api/token", "Method": "POST", "Description": ""},
-        {"Endpoint": "/api/token/refresh", "Method": "POST", "Description": ""},
+        {"Endpoint": "/api/token/refresh", "Method": "POST", "Description": "Allows for Token refresh within 60 days of last login"},
     ]
 
     return Response(routes)
+
+
+@api_view(["GET"])
+
+def get_notes(request):
+    user = request.user
+    notes = user.note_set.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
