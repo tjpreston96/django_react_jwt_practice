2ecd2b2cf781ad870ad25ccfa151f53bab5fbe61
Tyler Preston <tpreston96@gmail.com>
2022-07-12 22:08:16 -0500

    def __str__ to return first 45 characters

--- backend/base/models.py
+++ backend/base/models.py
@@ -8,3 +8,5 @@ class Note(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.body[0:45]
